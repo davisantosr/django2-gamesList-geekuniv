3ec5415432b0c0075b96b8c9cb1c9530ac1def32
davisantosr <davi-rocha2@hotmail.com>
2021-02-12 16:22:08 -0300

    first commit|app core|initial views

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+  return render(request, 'index.html')
+
+
+def contact(request):
+  return render(request, 'contact.html')
+
+
+def product(request):
+  return render(request, 'product.html')
+
+  
